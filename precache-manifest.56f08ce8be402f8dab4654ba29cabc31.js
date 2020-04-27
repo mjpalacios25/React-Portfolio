f3d143a939e2b6c56441297fee50466d8edc35d9
Moses Palacios <mospalacios@gmail.com>
2020-04-26 21:27:40 -0400

    Updates

---
+++ precache-manifest.56f08ce8be402f8dab4654ba29cabc31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f87bdbbb1ba85aae47dbc0a796ffd9b",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "9c7b1165abd0efaac7a9",
+    "url": "/React-Portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "afda21390b6b4cb282a8",
+    "url": "/React-Portfolio/static/js/2.262eeec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.262eeec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7b1165abd0efaac7a9",
+    "url": "/React-Portfolio/static/js/main.64f9bd2f.chunk.js"
+  },
+  {
+    "revision": "868e262c069a2b076aa5",
+    "url": "/React-Portfolio/static/js/runtime-main.ae9b118c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
